7d34d510aed43b7d75f1b263f3a10f49857601ea
zolbootbaatar <zolbootbaatar8@gmail.com>
2025-06-30 16:10:23 +0800

    udpate

--- src/views/pages/Terminal/TerminalMap.jsx
+++ src/views/pages/Terminal/TerminalMap.jsx
@@ -197,94 +197,84 @@ const TerminalMap = () => {
         </CAccordionItem> */}
 
         <CAccordionItem itemKey={3} alwaysOpen={true}>
-          <CAccordionHeader>TinCode & MerchantName</CAccordionHeader>
+          <CAccordionHeader>TinCode & Merchant names</CAccordionHeader>
           <CAccordionBody>
-            <CAccordionItem itemKey={1}>
-              <CAccordionHeader>TinCode & Merchant names</CAccordionHeader>
-              <CAccordionBody>
-                <CCol xs="auto">
-                  <CFormLabel
-                    className="visually-hidden"
-                    htmlFor="autoSizingInput"
+            <CCol xs="auto">
+              <CFormLabel className="visually-hidden" htmlFor="autoSizingInput">
+                Цахим баримт 3.0 TIN code ба ТТД Нэр авах
+              </CFormLabel>
+            </CCol>
+            <CCol xs={12}>
+              <CInputGroup className="mb-3">
+                <CFormInput
+                  className="reg"
+                  type="text"
+                  placeholder="Регистерийн дугаар"
+                  aria-describedby="button-addon2"
+                  value={regNo}
+                  onChange={handleInputChange}
+                />
+                <CButtonGroup role="group" aria-label="Basic example">
+                  <CButton
+                    type="button"
+                    color="primary"
+                    id="button-addon2"
+                    onClick={handleCheckUser}
                   >
-                    Цахим баримт 3.0 TIN code ба ТТД Нэр авах
-                  </CFormLabel>
-                </CCol>
-                <CCol xs={12}>
-                  <CInputGroup className="mb-3">
-                    <CFormInput
-                      className="reg"
-                      type="text"
-                      placeholder="Регистерийн дугаар"
-                      aria-describedby="button-addon2"
-                      value={regNo}
-                      onChange={handleInputChange}
-                    />
-                    <CButtonGroup role="group" aria-label="Basic example">
-                      <CButton
-                        type="button"
-                        color="primary"
-                        id="button-addon2"
-                        onClick={handleCheckUser}
-                      >
-                        Шалгах
-                      </CButton>
-                      <CButton color="primary" onClick={handleHideResults}>
-                        Хураах
-                      </CButton>
-                    </CButtonGroup>
-                  </CInputGroup>
-                </CCol>
-                <CCol xs={12}>
+                    Шалгах
+                  </CButton>
+                  <CButton color="primary" onClick={handleHideResults}>
+                    Хураах
+                  </CButton>
+                </CButtonGroup>
+              </CInputGroup>
+            </CCol>
+            <CCol xs={12}>
+              <CCollapse visible={visibleA}>
+                <CInputGroup className="mb-3">
+                  <CFormInput
+                    type="text"
+                    placeholder="Tin Code"
+                    aria-label="readonly input example"
+                    value={response?.data || ""}
+                    readOnly
+                  />
+                  <CFormInput
+                    type="text"
+                    placeholder="Merchant Name"
+                    aria-label="readonly input example"
+                    value={secondResponse?.data?.name || ""}
+                    readOnly
+                  />
+                </CInputGroup>
+              </CCollapse>
+            </CCol>
+            <CCol xs={12}>
+              <CRow>
+                <CCol>
                   <CCollapse visible={visibleA}>
-                    <CInputGroup className="mb-3">
-                      <CFormInput
-                        type="text"
-                        placeholder="Tin Code"
-                        aria-label="readonly input example"
-                        value={response?.data || ""}
-                        readOnly
-                      />
-                      <CFormInput
-                        type="text"
-                        placeholder="Merchant Name"
-                        aria-label="readonly input example"
-                        value={secondResponse?.data?.name || ""}
-                        readOnly
-                      />
-                    </CInputGroup>
+                    <CCard className="mt-3">
+                      <CCardBody>
+                        <div className="result">Анхны API-ийн хариу:</div>
+                        <pre>
+                          {response
+                            ? JSON.stringify(response, null, 2)
+                            : "No data"}
+                        </pre>
+                      </CCardBody>
+                      <CCardBody>
+                        <div className="result">Хоёр дахь API-ийн хариу:</div>
+                        <pre>
+                          {secondResponse
+                            ? JSON.stringify(secondResponse, null, 2)
+                            : "No data"}
+                        </pre>
+                      </CCardBody>
+                    </CCard>
                   </CCollapse>
                 </CCol>
-                <CCol xs={12}>
-                  <CRow>
-                    <CCol>
-                      <CCollapse visible={visibleA}>
-                        <CCard className="mt-3">
-                          <CCardBody>
-                            <div className="result">Анхны API-ийн хариу:</div>
-                            <pre>
-                              {response
-                                ? JSON.stringify(response, null, 2)
-                                : "No data"}
-                            </pre>
-                          </CCardBody>
-                          <CCardBody>
-                            <div className="result">
-                              Хоёр дахь API-ийн хариу:
-                            </div>
-                            <pre>
-                              {secondResponse
-                                ? JSON.stringify(secondResponse, null, 2)
-                                : "No data"}
-                            </pre>
-                          </CCardBody>
-                        </CCard>
-                      </CCollapse>
-                    </CCol>
-                  </CRow>
-                </CCol>
-              </CAccordionBody>
-            </CAccordionItem>
+              </CRow>
+            </CCol>
           </CAccordionBody>
         </CAccordionItem>
       </CAccordion>
